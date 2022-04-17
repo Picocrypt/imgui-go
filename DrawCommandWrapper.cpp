4df7eb2d47149483c8313c026fd04ce0e0c9ea1b
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2022-04-16 21:30:08 -0400

    Revert "Try to fix Unsafe pointer arithmetic issue"
    This reverts commit 68115946c5ccfe5d8a82ad91a49e683f6c23753d.

--- DrawCommandWrapper.cpp
+++ DrawCommandWrapper.cpp
@@ -1,34 +1,35 @@
+#include "imguiWrappedHeader.h"
 #include "DrawCommandWrapper.h"
 #include "WrapperConverter.h"
-#include "imguiWrappedHeader.h"
 
-unsigned int iggDrawCommandIdxOffset(IggDrawCmd handle) {
-  ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
-  return cmd->IdxOffset;
+void iggDrawCommandGetElementCount(IggDrawCmd handle, unsigned int *count)
+{
+   ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
+   *count = cmd->ElemCount;
 }
 
-void iggDrawCommandGetElementCount(IggDrawCmd handle, unsigned int *count) {
-  ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
-  *count = cmd->ElemCount;
+void iggDrawCommandGetClipRect(IggDrawCmd handle, IggVec4 *rect)
+{
+   ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
+   exportValue(*rect, cmd->ClipRect);
 }
 
-void iggDrawCommandGetClipRect(IggDrawCmd handle, IggVec4 *rect) {
-  ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
-  exportValue(*rect, cmd->ClipRect);
+void iggDrawCommandGetTextureID(IggDrawCmd handle, IggTextureID *id)
+{
+   ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
+   *id = cmd->GetTexID();
 }
 
-void iggDrawCommandGetTextureID(IggDrawCmd handle, IggTextureID *id) {
-  ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
-  *id = reinterpret_cast<IggTextureID>(cmd->TextureId);
+IggBool iggDrawCommandHasUserCallback(IggDrawCmd handle)
+{
+   ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
+   return (cmd->UserCallback != 0) ? 1 : 0;
 }
 
-IggBool iggDrawCommandHasUserCallback(IggDrawCmd handle) {
-  ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
-  return (cmd->UserCallback != 0) ? 1 : 0;
+void iggDrawCommandCallUserCallback(IggDrawCmd handle, IggDrawList listHandle)
+{
+   ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
+   ImDrawList *list = reinterpret_cast<ImDrawList *>(listHandle);
+   cmd->UserCallback(list, cmd);
 }
 
-void iggDrawCommandCallUserCallback(IggDrawCmd handle, IggDrawList listHandle) {
-  ImDrawCmd *cmd = reinterpret_cast<ImDrawCmd *>(handle);
-  ImDrawList *list = reinterpret_cast<ImDrawList *>(listHandle);
-  cmd->UserCallback(list, cmd);
-}
--- imguiWrapperTypes.h
+++ imguiWrapperTypes.h
@@ -1,14 +1,13 @@
 #pragma once
 
-#include <stdint.h>
-
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 typedef int IggBool;
 typedef float IggFloat;
-typedef uintptr_t IggTextureID;
+typedef void *IggTextureID;
 
 typedef void *IggContext;
 typedef void *IggDrawCmd;
@@ -23,18 +22,19 @@ typedef void *IggGuiStyle;
 typedef void *IggInputTextCallbackData;
 typedef void *IggIO;
 typedef void *IggFontGlyphRangesBuilder;
-typedef void *IggListClipper;
 
-typedef struct tagIggVec2 {
-  float x;
-  float y;
+typedef struct tagIggVec2
+{
+   float x;
+   float y;
 } IggVec2;
 
-typedef struct tagIggVec4 {
-  float x;
-  float y;
-  float z;
-  float w;
+typedef struct tagIggVec4
+{
+   float x;
+   float y;
+   float z;
+   float w;
 } IggVec4;
 
 #ifdef __cplusplus
