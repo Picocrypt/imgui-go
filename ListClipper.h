7f7a2c006a45681346ad591a5787ef91677e388f
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2022-04-16 21:18:36 -0400

    Update ListClipper.h

--- ListClipper.h
+++ ListClipper.h
@@ -3,23 +3,18 @@
 #include "imguiWrapperTypes.h"
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-typedef struct tagIggListClipper
-{
-   float StartPosY;
-   float ItemsHeight;
-   int ItemsCount;
-   int StepNo;
-   int DisplayStart;
-   int DisplayEnd;
-} IggListClipper;
+extern IggListClipper iggNewListClipper();
 
-extern IggBool iggListClipperStep(IggListClipper *clipper);
-extern void iggListClipperBegin(IggListClipper *clipper, int items_count, float items_height);
-extern void iggListClipperEnd(IggListClipper *clipper);
+extern int iggListClipperDisplayStart(IggListClipper handle);
+extern int iggListClipperDisplayEnd(IggListClipper handle);
+
+extern void iggListClipperDelete(IggListClipper handle);
+extern IggBool iggListClipperStep(IggListClipper handle);
+extern void iggListClipperBegin(IggListClipper handle, int items_count, float items_height);
+extern void iggListClipperEnd(IggListClipper handle);
 
 #ifdef __cplusplus
 }
